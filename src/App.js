2a3494478c5d30c8eb0af1fc813dadd0f2de53a4
onkardeolankar1996@gmail.com <codewithkeeda@gmail.com>
2022-07-24 11:58:26 +0530

    initial updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import './App.css';
 import Dashboard from './Page/Dashboard';
-import {useEffect, useState} from "react";
 
-const API = "https://employeenodejass.herokuapp.com/employees";
 
 function App() {
-  const [employeesData, setEmployeesData] = useState([]);
-  useEffect(() => {
-    fetch(`${API}`)
-    .then((data) => data.json())
-    .then((mbs) => setEmployeesData(mbs));
-  },[]);
-
   return (
     <div>
       <Dashboard />
