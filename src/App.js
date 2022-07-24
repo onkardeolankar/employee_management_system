a2b0c5cea31902e30e5b464572d5f2d3d9d84529
onkardeolankar1996@gmail.com <codewithkeeda@gmail.com>
2022-07-24 11:35:29 +0530

    employee data updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
 import Dashboard from './Page/Dashboard';
+import {useEffect, useState} from "react";
+
+const API = "https://employeenodejass.herokuapp.com/employees";
 
 function App() {
+  const [employeesData, setEmployeesData] = useState([]);
+  useEffect(() => {
+    fetch(`${API}`)
+    .then((data) => data.json())
+    .then((mbs) => setEmployeesData(mbs));
+  },[]);
+
   return (
     <div>
       <Dashboard />
